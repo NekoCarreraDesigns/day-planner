ba6658b3a0769c5a1a179b92bb5d5240e1b5cacd
NekoCarreraDesigns <denvermaas@gmail.com>
2020-04-29 00:19:13 -0600

    complete rewrite of js due to gross trump like incompetence

--- script.js
+++ script.js
@@ -1,116 +1,42 @@
+$(document).ready(function () {
+    var notes = [9, 10, 11, 12, 13, 14, 15, 16, 17];
+    var marked_Notes = "notes"
+    var now = moment().format("MM" + "/" + "DD" + "/" + "YYYY");
+    $("#currentDay").text(now);
 
-var currentTime = new Date();
-var day = currentTime.getDate();
-var month = currentTime.getMonth() + 1;
-var year = currentTime.getFullYear();
-var hour = currentTime.getHours();
-if (day < 10) {
-    day = "0" + day;
-}
-if (month < 10) {
-    month = "0" + month;
-}
-var todayDate = month + "/" + day + "/" + year;
-document.getElementById("currentDay").innerHTML = todayDate;
+    var hourEl = moment().get("hour");
 
-
-var notes = [];
-var marked_Notes = "notes"
-var blockTime = currentTime.hour
-var blockText
-
-function setBgColor(currentTime) {
-    blockTime = currentTime.split("")
-    blockText = textTime.split("")
-
-    if (blockTime < blockText) {
-        currentTime.addClass("past")
-    } else {
-        currentTime === blockTime
-        currentTime.addClass("present")
-    }
-    if (currentTime > blockTime) {
-        currentTime.addClass("future")
-    }
-
-
-}
-
-for (var i = 0; i < notes.length; i++) {
-
-}
+    $(".hour").each(function () {
+        var timeIndex = parseInt($(this).attr("value"));
+        if (timeIndex < hourEl) {
+            $(this).addClass("past");
+        }
+        if (timeIndex === hourEl) {
+            $(this).addClass("present");
+        }
+        if (timeIndex > hourEl) {
+            $(this).addClass("future");
+        }
 
 
 
-var saveButton9 = document.getElementById("9")
 
-var saveButton10 = document.getElementById("10")
+    });
 
-var saveButton11 = document.getElementById("11")
-
-var saveButton12 = document.getElementById("12")
-
-var saveButton1 = document.getElementById("13")
-
-var saveButton2 = document.getElementById("14")
-
-var saveButton3 = document.getElementById("15")
-
-var saveButton4 = document.getElementById("16")
-
-var saveButton5 = document.getElementById("17")
-
-
-
-saveButton9.addEventListener("click", saveInput)
-
-
-saveButton10.addEventListener("click", saveInput)
-
-
-saveButton11.addEventListener("click", saveInput)
-
-
-saveButton12.addEventListener("click", saveInput)
-
-
-saveButton1.addEventListener("click", saveInput)
-
-
-saveButton2.addEventListener("click", saveInput)
-
-
-saveButton3.addEventListener("click", saveInput)
-
-
-saveButton4.addEventListener("click", saveInput)
-
-
-saveButton5.addEventListener("click", saveInput)
-
-
-timeBlock = document.getElementsByClassName("time-block")
-function saveInput(pText, pId) {
-    nBlock = {
-        id: pId,
-        input: pText
-    }
-    for (var i = 0; i < marked_Notes.length; i++) {
-        if (marked_Notes[i].id === nBlock.id) {
-
-            localStorage.setItem(marked_Notes, JSON.stringify(notes));
-            return null;
+    for (var i = 0; i < notes.length; i++) {
+        if (notes === marked_Notes) {
+            localStorage.setItem(marked_Notes)
         }
     }
-    marked_Notes.push(nBlock)
-    localStorage.setItem(marked_Notes, JSON.stringify(marked_Notes))
-}
-saveInput()
+    $(".saveBtn").on("click", function (event) {
+
+        var clicked = $(event).prev().val()
+        var notes = JSON.stringify($(clicked).prev().val());
+        localStorage.setItem(notes, clicked);
+
+    })
+
+    localStorage.getItem(JSON.parse("marked_Notes"))
 
 
-
-
-
-
-
-
+});
